218e11df42308043c7beac853dc3634182b5e349
sagarr <sagar.rohankar@gmail.com>
2015-03-24 20:38:33 +0530

    added support for multiple args parsing using recursion

--- args/src/args/core.clj
+++ args/src/args/core.clj
@@ -8,11 +8,24 @@
 	)
 )
 
-(defn parse [schema, arg]
-	(let [type (schema (keyword (str (get arg 1)))) arg-len (.length arg)]
-		(if (and (> arg-len 3) (not (clojure.string/blank? (subs arg 3 arg-len))))
-			(read-string (subs arg 3 arg-len))
-			(default-value type)
+(defn internal-parse [schema arg-list values]
+	(if (or (nil? arg-list) (empty? arg-list))
+		values
+		(do ; else append to values
+			(internal-parse schema (rest arg-list)
+				(conj values
+					(let [type (schema (keyword (second (first arg-list)))) arg-len (.length (nth (first arg-list) 2))]
+						(if (and (> arg-len 3) (not (clojure.string/blank? (nth (first arg-list) 2))))
+							(read-string (clojure.string/trim (nth (first arg-list) 2)))
+							(default-value type)
+						)
+					)
+				)
+			)
 		)
 	)
+)
+
+(defn parse [schema arg]
+	(internal-parse schema (re-seq #"-([a-z])([a-z0-9 ]*)?" arg) [])
 )
--- args/test/args/core_test.clj
+++ args/test/args/core_test.clj
@@ -4,17 +4,23 @@
 
 (deftest parse-test
   (testing "parse single boolean argument w/o value"
-    (is (= (parse {:l boolean} "-l") true)))
+    (is (= (parse {:l boolean} "-l") [true])))
 
   (testing "parse single boolean argument w/ false value"
-    (is (= (parse {:l boolean} "-l false") false)))
+    (is (= (parse {:l boolean} "-l false") [false])))
 
   (testing "parse single boolean argument w/ true value"
-    (is (= (parse {:l boolean} "-l true") true)))
+    (is (= (parse {:l boolean} "-l true") [true])))
 
   (testing "parse single integer argument w/o value"
-    (is (= (parse {:p int} "-p") 1024)))
+    (is (= (parse {:p int} "-p") [1024])))
 
   (testing "parse single integer argument w/ 8080 value"
-    (is (= (parse {:p int} "-p 8080") 8080)))
+    (is (= (parse {:p int} "-p 8080") [8080])))
+
+  (testing "parse multiple argument w/o values"
+    (is (= (parse {:l boolean :p int} "-l -p") '[true 1024])))
+
+  (testing "parse multiple argument w/ values"
+    (is (= (parse {:l boolean :p int} "-l false -p 8080") '[false 8080])))
 )
