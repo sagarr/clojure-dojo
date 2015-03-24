1092dd01295523f0fb7836ce779b302a6f429611
sagarr <sagar.rohankar@gmail.com>
2015-03-24 20:49:28 +0530

    cleanup: tab to spaces, using :require, created let var for arg and arg-value

--- args/src/args/core.clj
+++ args/src/args/core.clj
@@ -1,31 +1,37 @@
 (ns args.core
+  (:require [clojure.string :as string])
   (:gen-class))
 
 (defn default-value [type]
-	(cond
-		(instance? clojure.core$boolean type) true
-		(instance? clojure.core$int type) 1024
-	)
+    (cond
+        (instance? clojure.core$boolean type) true
+        (instance? clojure.core$int type) 1024
+    )
 )
 
 (defn internal-parse [schema arg-list values]
-	(if (or (nil? arg-list) (empty? arg-list))
-		values
-		(do ; else append to values
-			(internal-parse schema (rest arg-list)
-				(conj values
-					(let [type (schema (keyword (second (first arg-list)))) arg-len (.length (nth (first arg-list) 2))]
-						(if (and (> arg-len 3) (not (clojure.string/blank? (nth (first arg-list) 2))))
-							(read-string (clojure.string/trim (nth (first arg-list) 2)))
-							(default-value type)
-						)
-					)
-				)
-			)
-		)
-	)
+    (if (or (nil? arg-list) (empty? arg-list))
+        values
+        (do ; else append to values
+            (internal-parse schema (rest arg-list)
+                (conj values
+                    (let [
+                            arg (first arg-list)
+                            arg-value (nth arg 2)
+                            type (schema (keyword (second arg)))
+                            arg-len (.length arg-value)
+                        ]
+                        (if (and (> arg-len 3) (not (string/blank? arg-value)))
+                            (read-string (string/trim arg-value))
+                            (default-value type)
+                        )
+                    )
+                )
+            )
+        )
+    )
 )
 
 (defn parse [schema arg]
-	(internal-parse schema (re-seq #"-([a-z])([a-z0-9 ]*)?" arg) [])
+    (internal-parse schema (re-seq #"-([a-z])([a-z0-9 ]*)?" arg) [])
 )
